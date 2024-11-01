92d4f4f4130cc830821f3c3d276b9a5dc7386ae0
Jacob Shaw <jacob.shaw@genesys.com>
2024-11-01 15:47:30 -0400

    make diff render data a state object

--- yeast-react-renderer/src/YeastNodeTypes/ImageNodeRenderer.tsx
+++ yeast-react-renderer/src/YeastNodeTypes/ImageNodeRenderer.tsx
@@ -5,6 +5,7 @@ import { useKey } from '../helpers/useKey';
 import { DiffRenderData, getDiffRenderData } from '../helpers/diff';
 import { ReactRenderer } from '../ReactRenderer';
 import CmsApi from '../helpers/types'
+import { LoadingPlaceholder } from 'genesys-react-components';
 
 interface IProps {
 	node: ImageNode;
@@ -17,40 +18,76 @@ const hostnameRegex = /^https?:\/\//i;
 export default function ImageNodeRenderer(props: IProps) {
 	const [imgSrc, setImgSrc] = useState<string>();
 	const [loadingError, setLoadingError] = useState<string>();
+	const [oldSrc, setOldSrc] = useState<string>();
+	const [newSrc, setNewSrc] = useState<string>();
+	const [oldAlt, setOldAlt] = useState<string>();
+	const [newAlt, setNewAlt] = useState<string>();
+	const [oldTitle, setOldTitle] = useState<string>();
+	const [newTitle, setNewTitle] = useState<string>();
+	const [diffRenderData, setDiffRenderData] = useState<DiffRenderData>();
 
 	const key1 = useKey();
 	const key2 = useKey();
 	const currentSrc = useRef<string>();
+	const currentNode = useRef<ImageNode>();
+
 
 	useEffect(() => {
-		if (currentSrc.current === props.node.src) return;
-		currentSrc.current = props.node.src;
-		(async () => {
-			try {
-				setLoadingError(undefined);
-				setImgSrc(undefined);
-				const match = hostnameRegex.exec(props.node.src);
-				let src = new URL(props.node.src, window.location.href);
-				const isSameHost = window.location.hostname.toLowerCase() === src.hostname.toLowerCase();
-				if (match && !isSameHost) {
-					// Set src to URL to let the browser load the image normally
-					setImgSrc(props.node.src);
-				} else {
-					// Load image from API and set src as encoded image data
-					const content = await props.api.getAssetContent(src.pathname, true);
-					if (!content) {
-						setLoadingError('Failed to load image');
-					}
-					let str = await readBlob(content?.content);
-					setImgSrc(str);
-				}
-			} catch (err) {
-				console.error(err);
-				setLoadingError('Failed to load image');
+		if (JSON.stringify(props.node) === JSON.stringify(currentNode)) return;
+
+		const newDiffRenderData: DiffRenderData = getDiffRenderData(props.node);
+		if (newDiffRenderData && newDiffRenderData.renderedStrings) {
+			if (newDiffRenderData.renderedStrings['title']) {
+				setOldTitle(newDiffRenderData.renderedStrings['title'].oldString);
+				setNewTitle(newDiffRenderData.renderedStrings['title'].newString);
 			}
-		})();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [props.node.src]);
+			if (newDiffRenderData.renderedStrings['alt']) {
+				setOldAlt(newDiffRenderData.renderedStrings['alt'].oldString);
+				setNewAlt(newDiffRenderData.renderedStrings['alt'].newString);
+			}
+			if (newDiffRenderData.renderedStrings['src']) {
+				(async () => {
+					const oldImgSrc = await getImgSrc(newDiffRenderData.renderedStrings['src'].oldString);
+					if (oldImgSrc) setOldSrc(oldImgSrc);
+					const newImgSrc = await getImgSrc(newDiffRenderData.renderedStrings['src'].newString);
+					if (newImgSrc) setNewSrc(newImgSrc);
+				})();
+			}
+
+			setDiffRenderData(newDiffRenderData);
+		} else if (currentSrc.current !== props.node.src) {
+			currentSrc.current = props.node.src;
+			(async () => {
+				const newSrc = await getImgSrc(props.node.src);
+				if (newSrc) setImgSrc(newSrc)
+			})();
+		}
+	}, [props.node]);
+
+	const getImgSrc = async (src: string): Promise<string | undefined> => {
+		try {
+			setLoadingError(undefined);
+			setImgSrc(undefined);
+			const match = hostnameRegex.exec(src);
+			let newSrc = new URL(src, window.location.href);
+			const isSameHost = window.location.hostname.toLowerCase() === newSrc.hostname.toLowerCase();
+			if (match && !isSameHost) {
+				// Set src to URL to let the browser load the image normally
+				return src;
+			} else {
+				// Load image from API and set src as encoded image data
+				const content = await props.api.getAssetContent(newSrc.pathname, true);
+				if (!content) {
+					setLoadingError('Failed to load image');
+				}
+				let str = await readBlob(content?.content);
+				return str;
+			}
+		} catch (err) {
+			console.error(err);
+			setLoadingError('Failed to load image');
+		}
+	};
 
 	const readBlob = async(imageBlob: Blob) => {
 		const reader = new FileReader();
@@ -58,39 +95,17 @@ export default function ImageNodeRenderer(props: IProps) {
 			reader.onloadend = () => resolve(reader.result as string);
 			reader.readAsDataURL(imageBlob);
 		});
-	}
+	};
 
-	const diffRenderData: DiffRenderData = getDiffRenderData(props.node);
 	const className: string = diffRenderData ? diffRenderData.diffClass : '';
 
-	let oldSrc: string = '';
-	let newSrc: string = '';
-	let oldAlt: string = '';
-	let newAlt: string = '';
-	let oldTitle: string = '';
-	let newTitle: string = '';
-
-	if (diffRenderData && diffRenderData.renderedStrings) {
-		if (diffRenderData.renderedStrings['title']) {
-			oldTitle = diffRenderData.renderedStrings['title'].oldString;
-			newTitle = diffRenderData.renderedStrings['title'].newString;
-		}
-		if (diffRenderData.renderedStrings['alt']) {
-			oldAlt = diffRenderData.renderedStrings['alt'].oldString;
-			newAlt = diffRenderData.renderedStrings['alt'].newString;
-		}
-		if (diffRenderData.renderedStrings['src']) {
-			oldSrc = diffRenderData.renderedStrings['src'].oldString;
-			newSrc = diffRenderData.renderedStrings['src'].newString;
-		}
-	}
-
 	return diffRenderData && diffRenderData.renderedStrings
-		? (
+		?
 			<React.Fragment>
 				<img key={key1.current} alt={oldAlt} src={oldSrc} title={oldTitle} className={className}/>
 				<img key={key2.current} alt={newAlt} src={newSrc} title={newTitle} className={className}/>
 			</React.Fragment>
-		)
-		: <img key={key1.current} alt={props.node.alt} src={props.node.src} title={props.node.title} className={className} />
+		: imgSrc
+			? <img key={key1.current} alt={props.node.alt} src={imgSrc} title={props.node.title} className={className} />
+			: <LoadingPlaceholder />;
 }
