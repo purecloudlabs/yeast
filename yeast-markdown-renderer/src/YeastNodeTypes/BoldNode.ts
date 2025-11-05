486cafe824489c5f4e80a96d5d37d8d22ae984e5
Min Young Park <Min_Young.Park@genesys.com>
2025-11-05 11:26:28 -0500

    small change

--- yeast-markdown-renderer/src/YeastNodeTypes/BoldNode.ts
+++ yeast-markdown-renderer/src/YeastNodeTypes/BoldNode.ts
@@ -2,5 +2,5 @@ import { MarkdownRenderer } from '../MarkdownRenderer';
 import { BoldNode } from 'yeast-core';
 
 export default function renderBoldNode(node: BoldNode, renderer: MarkdownRenderer) {
-	return `**${renderer.renderComponents(node.children).join('')}**`;
+	return `**${renderer.renderComponents(node.children).join('').trim()}**`;
 }
